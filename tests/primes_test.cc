b4f02d9b71e1f40200872d2ec4f4494b5c9189d7
Péter Pallos <pallosp@gmail.com>
2024-12-06 09:37:13 +0100

    Unit test the Miller-Rabin algorithm

--- tests/primes_test.cc
+++ tests/primes_test.cc
@@ -12,6 +12,15 @@ TEST(Primes, NaiveAlgorithm) {
   EXPECT_FALSE(is_prime_naive(4));
 }
 
+TEST(Primes, MillerRabin) {
+  EXPECT_FALSE(is_prime_miller_rabin(0));
+  EXPECT_FALSE(is_prime_miller_rabin(1));
+  EXPECT_TRUE(is_prime_miller_rabin(2));
+  EXPECT_TRUE(is_prime_miller_rabin(3));
+  EXPECT_FALSE(is_prime_miller_rabin(4));
+  EXPECT_TRUE(is_prime_miller_rabin(INT_MAX));
+}
+
 TEST(Primes, AlgorithmsAreEquivalent) {
   for (int i = 0; i < 100; i++) {
     EXPECT_EQ(is_prime_naive(i), is_prime_miller_rabin(i))
