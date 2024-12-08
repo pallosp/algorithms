03b6a80d7fd03b2ae70f3080fc1ace2cf9f95b95
Péter Pallos <pallosp@gmail.com>
2024-12-08 08:53:03 +0100

    Declare a struct for 2D points

---
+++ src/point.h
@@ -0,0 +1,18 @@
+#include <ostream>
+
+// Point on the Euklidean plane.
+struct Point {
+  int x;
+  int y;
+
+  Point(int x, int y) {
+    this->x = x;
+    this->y = y;
+  }
+
+  bool operator==(const Point &p) const { return x == p.x && y == p.y; }
+
+  friend std::ostream &operator<<(std::ostream &stream, const Point &point) {
+    return stream << "(" << point.x << "," << point.y << ")";
+  }
+};
